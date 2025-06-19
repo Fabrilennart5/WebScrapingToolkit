7063b9f19efa7754f5f96bbfe23d79b4742bb054
Fabricio <Fabricio.Flores0125@jala.university>
2025-06-19 10:26:06 -0400

    nuevas clases agregadas

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/media/fabri/Toshiba Disk/Proyectos /WebScrapingToolkit/.venv"
+set -gx VIRTUAL_ENV "/persistent/home/fabri/proyectos/WebScrapingToolkit/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
